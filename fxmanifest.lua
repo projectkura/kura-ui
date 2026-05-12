9d1745eef1a2278e96143bc82d71a91139b2182e
acontxt <har.niklas7@proton.me>
2026-05-12 12:08:44 +0200

    chore: track fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+name 'kura-ui'
+author 'Walteria.net'
+version '0.1.0'
+license 'LGPL-3.0-or-later'
+description 'Kura UI interface layer for Project Kura'
+
+ui_page 'web/build/index.html'
+
+files {
+    'web/build/index.html',
+    'web/build/**/*',
+}
+
+shared_scripts {
+    '@kura-lib/init.lua',
+    '@kura-core/init.lua',
+    'init.lua',
+}
+
+client_scripts {
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/*.lua',
+}
